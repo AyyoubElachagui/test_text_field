2778ddf5b0092b565ccbd3d33116d6b69138e55d
AyyoubElachagui <best_way_development@outlook.fr>
2021-11-13 18:09:52 +0100

    test TextField

---
+++ lib/src/second_test/second.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:text_selection_controls/text_selection_controls.dart';
+
+class SecondTest extends StatefulWidget {
+  @override
+  _SecondTestState createState() => _SecondTestState();
+}
+
+class _SecondTestState extends State<SecondTest> {
+  @override
+  Widget build(BuildContext context) {
+    return S;
+  }
+}
